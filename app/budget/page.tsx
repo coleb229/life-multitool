92fe56a1e6e64ffe2da3d241c85292d3d2798e2e
Cole Brant <coleb@Coles-Laptop.lan>
2024-09-20 14:10:36 -0500

    fixed padding issue on mobile

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -36,7 +36,7 @@ export default async function BudgetPage() {
   }  
 
   return (
-    <div className="container mx-auto p-20">
+    <div className="container mx-auto p-0 md:p-24">
       <h1 className="text-3xl font-bold text-center mb-6">Budget Management</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className='py-20'>
