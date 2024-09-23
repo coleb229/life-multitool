d91d2214bff94d3bb3ae6cd5be6e9ae3153fe669
Cole Brant <coleb@Coles-Laptop.lan>
2024-09-23 04:39:52 -0500

    styled the tiptap output on the chapter page

--- app/journal/[book]/[chapter]/page.tsx
+++ app/journal/[book]/[chapter]/page.tsx
@@ -12,7 +12,7 @@ export default async function Page({ params } : { params: { chapter: string } })
   return (
     <div className="p-24">
       <h1 className="text-primary ml-8 mb-2 font-semibold text-2xl">{chapter?.title}</h1>
-      <div className="min-h-screen bg-[#f2f2f2] rounded-lg text-black p-10" dangerouslySetInnerHTML={{ __html: chapter?.content as string }} />
+      <div className="min-h-screen bg-[#f2f2f2] rounded-lg text-black p-10 tiptap" dangerouslySetInnerHTML={{ __html: chapter?.content as string }} />
       <UpdateChapterContent chapterId={params.chapter} data={chapter} />
     </div>
   );
